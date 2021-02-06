e4556363a4856daefd39ae1689548d1d98939d45
Abdul Muttaqin <abdulmuttaqin456@gmail.com>
2021-02-06 18:30:03 +0700

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const axios = require('axios')
+const cheerio = require('cheerio');
+
+// Mendefinisikan header
+
+const config = {
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+}
+
+function ayat(kata) {
+    return new Promise(resolve => {
+        var url = "https://laqin-wap.herokuapp.com/web/search.php?q=" + kata;
+        axios.get(url, config)
+            .then((result) => {
+                let $ = cheerio.load(result.data);
+                let ig = [];
+
+                $('div.aya_container').each(function (i, e) {
+                    ig.push({
+                        arab: $('div.aya_text').text(),
+                        indo: $('div.aya_trans').text()
+                    })
+                });
+                resolve(ig)
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    });
+}
+
+exports.ayat = ayat;
